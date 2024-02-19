f6ea3937bfb92ad239aeeea36558730db089bec4
BHATTILAKHANSINGH <jaswanthsinghjassi@gmail.com>
2024-02-19 12:59:30 +0530

    add these to master

--- src/main.py
+++ src/main.py
@@ -4,3 +4,6 @@ added fix for sprint-2
 sprint-2 development started
 (sprint-2 development started)
 (sprint-2 development started)
+0f45949 (sprint-2 development started)
+added these to master
+aa15699 (add these to master)
